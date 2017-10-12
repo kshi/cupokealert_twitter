51b677720af9238f767e355c21ad9c444ad9f446
Kevin <kshi@cs.columbia.edu>
2017-10-12 17:15:08 -0400

    formatting output changes

--- cupokealert.py
+++ cupokealert.py
@@ -40,30 +40,29 @@ for tweet in reversed(tweets):
     last_query_id = tweet.id
     t_json = tweet._json
     text = t_json['text']
-    for keyword in keywords:
-        if keyword in text:
-            gmapurl = t_json['entities']['urls'][1]['expanded_url']
-            components = re.split('=|,', gmapurl)
-            lattitude = float(components[1])
-            longitude = float(components[2])
-            distance = dist_in_km(lattitude, longitude)
-            if distance < max_km:
-                text = re.sub('\[.+\] ', '', text)
-                text_array = text.split(' ')
-                pokemon = text_array[0]
-                url_nycpokemap = "<" + text_array[-2] + ">"
-                url_googlemap = "<" + text_array[-1] + ">"
-                text = ' '.join(text_array[0:-2])
-                iv_text = re.search('IV: \d+%', text)
-                cp_text = re.search('CP: \d+\)', text)
-                iv = "( " + iv_text.group(0) + " )" if iv_text else ''
-                cp = "( " + cp_text.group(0).replace(')','') + " )" if cp_text else ''                
-                data = re.search('until.+',text).group(0)
-                content = pokemon + ' ' + iv + ' ' + cp  + ' ' + data + ' ( ' + str(distance) + ' km away )' + '\n' + url_nycpokemap + '  ' + url_googlemap
-                webhook = Webhook(credentials.discord_webhook, content, discord_username, discord_avatar)
-                webhook.format()
-                webhook.post()
-                break
+    if any(keyword in text for keyword in keywords):
+        gmapurl = str(t_json['entities']['urls'][1]['expanded_url'])
+        nycpokemapurl = str(t_json['entities']['urls'][0]['expanded_url'])
+        components = re.split('=|,', gmapurl)
+        lattitude = float(components[1])
+        longitude = float(components[2])
+        distance = dist_in_km(lattitude, longitude)
+        if distance < max_km:
+            text = re.sub('\[.+\] ', '', text)
+            text_array = text.split(' ')
+            pokemon = text_array[0]
+            url_nycpokemap = "<" + nycpokemapurl + ">"
+            url_googlemap = "<" + gmapurl + ">"
+            text = ' '.join(text_array[0:-2])
+            iv_text = re.search('IV: \d+%', text)
+            cp_text = re.search('CP: \d+\)', text)
+            iv = "( " + iv_text.group(0) + " )" if iv_text else ''
+            cp = "( " + cp_text.group(0).replace(')','') + " )" if cp_text else ''                
+            data = re.search('until.+',text).group(0)
+            content = pokemon + ' ' + iv + ' ' + cp  + ' ' + data + ' ( ' + str(distance) + ' km away )' + '\n' + url_nycpokemap + '\n' + url_googlemap
+            webhook = Webhook(credentials.discord_webhook, content, discord_username, discord_avatar)
+            webhook.format()
+            webhook.post()
 
 params = open(param_loc,"w")
 params.write(str(last_query_id))
